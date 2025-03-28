ffbd2bee3bc96ab78fb80131dbaf07d2546122c1
Rajeevthomas <rajeevthomassaganty@gmail.com>
2025-03-27 23:25:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -52,4 +52,5 @@ def get_file(filename):
     return send_file('./files/' + filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))  # Default to port 8080
+    app.run(host="0.0.0.0", port=port)
